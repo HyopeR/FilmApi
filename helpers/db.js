c51cfd920339c56f0aab0ee293d61091dcd8387f
HyopeR <tol_celik@hotmail.com>
2020-07-27 07:27:31 +0300

    db.js comment line updated

--- helpers/db.js
+++ helpers/db.js
@@ -1,18 +1,18 @@
 const {Pool, Client} = require('pg');
-const client = new Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    sslmode: process.env.SSL_MODE
-});
-
 // const client = new Client({
-//     connectionString: process.env.CONNECTION_STRING,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     sslmode: process.env.SSL_MODE
 // });
+
+const client = new Client({
+    connectionString: process.env.CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 module.exports = client;
