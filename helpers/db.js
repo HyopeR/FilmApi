59f63eb06b40d76831597b0e00d065bb84af7926
HyopeR <tol_celik@hotmail.com>
2020-12-28 22:59:05 +0300

    db config updated

--- helpers/db.js
+++ helpers/db.js
@@ -1,22 +1,24 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.LOCAL_DB_NAME, process.env.LOCAL_DB_USER, process.env.LOCAL_DB_PASSWORD, {
-    dialect: 'postgres',
-    host: process.env.LOCAL_DB_HOST,
-    port: process.env.LOCAL_DB_PORT,
-    logging: false,
-});
-
-// const db = new Sequelize(process.env.NETWORK_CONNECTION_STRING, {
-//   dialect: 'postgres',
-//   protocol: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   },
-//   logging: false,
+// const db = new Sequelize(process.env.LOCAL_DB_NAME, process.env.LOCAL_DB_USER, process.env.LOCAL_DB_PASSWORD, {
+//     dialect: 'postgres',
+//     host: process.env.LOCAL_DB_HOST,
+//     port: process.env.LOCAL_DB_PORT,
+//     logging: false,
 // });
+
+const db = new Sequelize(process.env.NETWORK_DB_NAME, process.env.NETWORK_DB_USER, process.env.NETWORK_DB_PASSWORD, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
+
+  logging: false,
+  host: process.env.NETWORK_DB_HOST,
+  port: process.env.NETWORK_DB_PORT,
+});
 
 module.exports = db;
