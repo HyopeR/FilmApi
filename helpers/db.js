9f402b43c1e31b2dbcb1deda95833a40f85a2485
HyopeR <tol_celik@hotmail.com>
2020-12-28 23:13:11 +0300

    db config change local

--- helpers/db.js
+++ helpers/db.js
@@ -1,18 +1,19 @@
-const { Sequelize } = require('sequelize');
+const {Sequelize} = require('sequelize');
 
-// const db = new Sequelize(process.env.LOCAL_DB_NAME, process.env.LOCAL_DB_USER, process.env.LOCAL_DB_PASSWORD, {
-//     dialect: 'postgres',
-//     host: process.env.LOCAL_DB_HOST,
-//     port: process.env.LOCAL_DB_PORT,
-//     logging: false,
-// });
-
-const db = new Sequelize('postgres://bxshxcrnvobulg:1df8910952e1a96e4fd85256865fb2ee40f1c4ed8e4c9b283aaf2f4e1a473dc2@ec2-52-204-232-46.compute-1.amazonaws.com:5432/defa3n14i9ph68', {
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  },
+const db = new Sequelize(process.env.LOCAL_DB_NAME, process.env.LOCAL_DB_USER, process.env.LOCAL_DB_PASSWORD, {
+    dialect: 'postgres',
+    host: process.env.LOCAL_DB_HOST,
+    port: process.env.LOCAL_DB_PORT,
+    logging: false,
 });
+
+// const db = new Sequelize(process.env.NETWORK_CONNECTION, {
+//   dialectOptions: {
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   },
+//   logging: false,
+// });
 
 module.exports = db;
